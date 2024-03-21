7cebae46f048e1973840cbb1fb24c17cb4956b5b
Honza Filipsky <honzaf@4max.com>
2024-03-21 16:13:48 +0100

    still crashes on Android in release mode, another blind fix

--- hid/example/lib/main.dart
+++ hid/example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hid/hid.dart';
 
@@ -20,7 +22,9 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    _listDevices();
+    Future.delayed(const Duration(milliseconds: 100), () {
+      _listDevices();
+    });
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -29,7 +33,7 @@ class _MyAppState extends State<MyApp> {
       _hidDevices = null;
     });
 
-    final hidDevices = await getDeviceList();
+    final hidDevices = await getDeviceList(vendorId: null, productId: null);
     hidDevices.sort((a, b) => a.usage?.compareTo(b.usage ?? 0) ?? 0);
     hidDevices.sort((a, b) => a.usagePage?.compareTo(b.usagePage ?? 0) ?? 0);
     hidDevices.sort((a, b) => a.productId.compareTo(b.productId));
@@ -41,9 +45,71 @@ class _MyAppState extends State<MyApp> {
     // setState to update our non-existent appearance.
     if (!mounted) return;
 
+    _operateTheRelay();
+
     setState(() {
       _hidDevices = hidDevices;
     });
+  }
+
+  Future<bool> _operateTheRelay() async {
+    if (!await _openTheRelay(true, 0)) {
+      return false;
+    }
+    await _openTheRelay(true, 1);
+
+    var states = await _getPortStates();
+
+    await Future.delayed(const Duration(seconds: 2));
+    await _openTheRelay(false, 0);
+    await _openTheRelay(false, 1);
+
+    states = await _getPortStates();
+
+    return true;
+  }
+
+  Future<bool> _openTheRelay(bool switchOn, int portNo) async {
+    final relayBoard = (await getDeviceList(vendorId: 5824, productId: 1503)).firstOrNull;
+
+    if (relayBoard == null) {
+      return false;
+    }
+
+    await relayBoard.open();
+    final bytes = Uint8List.fromList([0x00, switchOn ? 0xff : 0xfd, portNo + 1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+    await relayBoard.setFeature(bytes);
+    await relayBoard.close();
+    return true;
+  }
+
+  Future<List<bool>> _getPortStates() async {
+    final relayBoard = (await getDeviceList(vendorId: 5824, productId: 1503)).firstOrNull;
+
+    final result = <bool>[];
+
+    if (relayBoard == null) {
+      return result;
+    }
+
+    try {
+      final portCount = int.tryParse(relayBoard.productName.replaceAll("USBRelay", "")) ?? 0;
+
+      await relayBoard.open();
+
+      var bytes = Uint8List.fromList([1, 0, 0, 0, 0, 0, 0, 0, 0]);
+      await relayBoard.getFeature(bytes);
+
+      for (var portNo = 0; portNo < portCount; portNo++) {
+        final state = (bytes[8] & (1 << portNo)) != 0;
+        result.add(state);
+      }
+
+      return result;
+    } finally {
+      await relayBoard.close();
+    }
   }
 
   _getUsagePageIcon(int? usagePage, int? usage) {
@@ -92,8 +158,7 @@ class _MyAppState extends State<MyApp> {
             : ListView.builder(
                 itemCount: dev.length,
                 itemBuilder: (context, index) => ListTile(
-                  leading: Icon(_getUsagePageIcon(
-                      dev[index].usagePage, dev[index].usage)),
+                  leading: Icon(_getUsagePageIcon(dev[index].usagePage, dev[index].usage)),
                   title: Text(dev[index].productName),
                   subtitle: Text(
                       '${dev[index].vendorId.toRadixString(16).padLeft(4, '0')}:${dev[index].productId.toRadixString(16).padLeft(4, '0')}   ${dev[index].serialNumber}'),
